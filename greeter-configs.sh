fefbd891942f7239f41036dd6d8722bea96af2d4
Pol-Jak-295 <122225928+Pol-Jak-295@users.noreply.github.com>
2026-02-25 10:48:04 +0100

    Update greeter-configs.sh
    add the ani-cli executable to the greeter configs.

--- greeter-configs.sh
+++ greeter-configs.sh
@@ -6,4 +6,6 @@ sudo cp -r $REPO_DIR/images/frieren-staff-3840x2160.jpg /usr/share/wallpaper/fri
 sudo cp -r $REPO_DIR/etc/sddm.conf /etc/sddm.conf
 sudo mkdir /usr/share/sddm/themes/
 sudo cp -r $REPO_DIR/sugar-candy/ /usr/share/sddm/themes/
+sudo cp $REPO_DIR/ani-cli/ani-cli /usr/bin/ani-cli
+sudo chmod +x /usr/bin/ani-cli 
 echo "Done."
