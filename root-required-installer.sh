4db23b91acce16dedbb11cd62db405009eb92ab3
Pol-Jak-295 <polesnik.jaka@gmail.com>
2026-04-06 20:39:48 +0200

    update: update: updated ani-cli to be the newest version fix: fixed the root installer to hopefully install stuff correctly now (hopefully)

--- root-required-installer.sh
+++ root-required-installer.sh
@@ -1,12 +1,12 @@
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "installing greeter configurations..."
-sudo mkdir /usr/share/wallpaper
-sudo cp -r $REPO_DIR/images/frieren-staff-3840x2160.jpg /usr/share/wallpaper/frieren-staff-3840x2160.jpg
+sudo mkdir /usr/share/wallpapers
+sudo cp -r $REPO_DIR/images/frierens-staff-3840x2160.jpg /usr/share/wallpapers/frierens-staff-3840x2160.jpg
 sudo cp -r $REPO_DIR/etc/sddm.conf /etc/sddm.conf
 sudo mkdir /usr/share/sddm/themes/
 sudo cp -r $REPO_DIR/sugar-candy/ /usr/share/sddm/themes/
-sudo cp $REPO_DIR/ani-cli/ani-cli /usr/bin/ani-cli
+sudo cp -rf $REPO_DIR/ani-cli/ani-cli /usr/bin/ani-cli
 sudo chmod +x /usr/bin/ani-cli 
 sudo ln -sf $REPO_DIR/nwg-bar/icons /usr/share/nwg-bar/images/icons
 echo "Done."
